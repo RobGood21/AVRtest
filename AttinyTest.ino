fa58aa5d53710ab5dd28e9bb34d19e5ec7ac4b7e
Rob Antonisse <robantonisse@gmail.com>
2019-07-05 22:37:44 +0200

    avr

--- AttinyTest.ino
+++ AttinyTest.ino
@@ -5,14 +5,15 @@
 */
 
 
-void setup()
-{
-
+void setup(){
+	//pinMode(0, OUTPUT);
 
 }
 
 void loop()
 {
-
-
+	//digitalWrite(0, HIGH);
+	//delay(100);
+	//digitalWrite(0, LOW);
+	//delay(2000);
 }
